5681e10e23e925e9fe5a219407744d028a779bdb
Demismoran <Demismoran8@gmail.com>
2022-01-05 16:42:09 -0800

    new

---
+++ java.js
@@ -0,0 +1,23 @@
+var one = 3 ;
+
+var two = 2 ;
+
+var three = one * two ;
+
+console.log(three);
+
+var num = 3 ;
+
+var num_two = 2 ;
+
+var num_three = num + num_two ;
+
+console.log(num_three);
+
+var string_one = 'My favorite food is ';
+
+var string_two = 'wingstop';
+
+var string_three = string_one + string_two;
+
+console.log(string_three);
