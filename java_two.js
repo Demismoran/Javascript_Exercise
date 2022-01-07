e8bc9f88e9547d0aa2b22376a7144dd9e239d825
Demismoran <Demismoran8@gmail.com>
2022-01-06 22:33:25 -0800

    first

---
+++ java_two.js
@@ -0,0 +1,17 @@
+var hoursOfSleep = 8 ;
+
+
+if ( hoursOfSleep <= 6) {
+	console.log("i am groggy");
+} else {
+	console.log("i feel fantastic");
+}
+
+
+var nextLocation = "home";
+
+if (nextLocation === home) {
+	console.log("Its been a long day, lets go home")
+} else {
+	console.log("Good morning, finding the fastest route to work!" )
+}
